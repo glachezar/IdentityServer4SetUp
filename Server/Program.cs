c6edac4dfc752d9e3f4a6c72f0eeef9f41e6909f
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-09 11:34:42 +0200

    Added IdentityServer4 QuickSatrt librery from GitHub that includes Views, wwwroot and Quickstart folder with controllers. Also Program.cs configuration for quickstart.

--- Server/Program.cs
+++ Server/Program.cs
@@ -50,9 +50,14 @@ namespace Server
                 .AddDeveloperSigningCredential();
 
             var app = builder.Build();
-
+            app.UseStaticFiles();
+            app.UseRouting();
             app.UseIdentityServer();
-
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
             app.Run();
         }
     }
