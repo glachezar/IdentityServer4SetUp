15c34bb274b22a51ac8abf59ef5d5a8ef98f545f
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-09 11:37:27 +0200

    Added login view model and register view model in Data folder.

---
+++ Server/Data/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Server.Data
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+
+        public string ReturnUrl { get; set; }
+    }
+}
