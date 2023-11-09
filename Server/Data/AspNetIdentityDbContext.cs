c522bf625bc4475d4690b6f30aba64b22c194951
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-09 10:22:49 +0200

    Added AspNetIdentityDbContext

---
+++ Server/Data/AspNetIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AspNetIdentityDbContext : IdentityDbContext
+    {
+        public AspNetIdentityDbContext(DbContextOptions<AspNetIdentityDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
