5043f83e7c759dd4f822fdd74da3522ecdb5d4f3
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-09 09:52:46 +0200

    Registering operation store and configuration store for IdentityServer in Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,36 @@
 namespace Server
 {
+    using System.Reflection;
+    using Microsoft.EntityFrameworkCore;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var assembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+            var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddIdentityServer()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(defaultConnectionString,
+                            opt => opt.MigrationsAssembly(assembly));
+
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(defaultConnectionString,
+                            opt => opt.MigrationsAssembly(assembly));
+                })
+                .AddDeveloperSigningCredential();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseIdentityServer();
 
             app.Run();
         }
