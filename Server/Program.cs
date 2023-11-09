85d1f35289d1803dfae4ad50649a55c31caf2c07
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-09 11:04:49 +0200

    Setup IdentityServer Seeding process in Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,10 +9,21 @@ namespace Server
     {
         public static void Main(string[] args)
         {
+            var seed = args.Contains("/seed");
+            if (seed)
+            {
+                args = args.Except(new[] { "/seed" }).ToArray();
+            }
+
             var builder = WebApplication.CreateBuilder(args);
 
             var assembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
             var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            if (seed)
+            {
+                SeedData.EnsureSeedData(defaultConnectionString);
+            }
 
             builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
                 options.UseSqlServer(defaultConnectionString,
